d153b340f34ebac7b59faf0edfaf41d0f7b4c222
root <root@kjani.rdu.csb>
2016-05-29 16:44:18 -0400

    created view for albums

--- music/views.py
+++ music/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from .models import  Album
 
 def index(request):   # request here is an HTTP request
-    return HttpResponse('<h1>This is the Music Homepage </h1>')
+    all_albums = Album.objects.all()    #all_albums is a var to store all albums
+    html = ''
+    for album in all_albums:
+        url = '/music/' + str(album.id) + '/'
+        html += '<a href=" ' + url + ' ">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
