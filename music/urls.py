e0343f8cd605490e90cc2b406b413d35a8f6584f
root <root@kjani.rdu.csb>
2016-05-29 16:10:25 -0400

    created 'detail' view

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views  # . is current directory
 
 urlpatterns = [
+    # /music/
     url(r'^$', views.index, name='index'),  #look for function named 'index' in views.py
+
+    # /music/712/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
 
-def index(request):
+def index(request):   # request here is an HTTP request
     return HttpResponse('<h1>This is the Music Homepage </h1>')
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
