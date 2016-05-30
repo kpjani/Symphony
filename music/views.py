36d0faaf756216263f7af41b7a1d47838ac64e1b
root <root@kjani.rdu.csb>
2016-05-29 22:21:04 -0400

    added Http404 error

--- music/views.py
+++ music/views.py
@@ -1,11 +1,15 @@
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import  Album
 
 def index(request):   # request here is an HTTP request
     all_albums = Album.objects.all()    #all_albums is a var to store all albums
-    context = {'all_albums' : all_albums}
+    context = {'all_albums' : all_albums}  #context is just a user defined name for a dictionary
     return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + "</h2>")
+      try:
+          album  = Album.objects.get(pk=album_id)
+      except Album.DoesNotExist:
+          raise Http404("Album does not exist")
+      return render(request, 'music/detail.html', {'album' : album})
