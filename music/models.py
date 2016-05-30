9199fd9f7e241c6f2e7687b98341a0623b128669
kpjani <kpjani@ncsu.edu>
2016-05-30 18:07:57 -0400

    changes to model

--- music/models.py
+++ music/models.py
@@ -12,9 +12,12 @@ class Album(models.Model):
     album_logo = models.CharField(max_length=1000)
 
     def __str__(self):
-        return self.album_title + ' - '+ self.artist
+        return self.album_title + ' - '+ self.artist  # Alum displays album title and artist
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.song_title  # Song displays song title
