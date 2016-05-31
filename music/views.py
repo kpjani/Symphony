658a0fa020cda0ce2b7ab934ca991298a6ffbd88
kpjani <kpjani@ncsu.edu>
2016-05-30 20:45:34 -0400

    added a form for fav. song

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from .models import  Album
+from .models import  Album, Song
+
 
 def index(request):   # request here is an HTTP request
     all_albums = Album.objects.all()    #all_albums is a var to store all albums
@@ -15,3 +16,17 @@ def detail(request, album_id):
       #the below 1 line replaces this try except block
       album = get_object_or_404(Album, pk=album_id)
       return render(request, 'music/detail.html', {'album' : album})
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album' : album,
+            'error_message' : "You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
